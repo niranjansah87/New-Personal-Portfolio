8cbdb8a124acb4939d86c75488caaf3b333316ff
Niranjan <2100090187@kluniversity.in>
2024-02-14 23:55:22 +0530

    made hero image responsive

--- src/components/Hero/styles.ts
+++ src/components/Hero/styles.ts
@@ -16,30 +16,28 @@ export const Container = styled.section`
     }
 
     h3{
-      color:var(--green);
+      color: var(--green);
       margin: 3rem 0;
     }
-
     
     p.small-resume {
       margin-bottom: 5rem;
     }
   }
-// New added
+
   .social-media{
     display: inline-flex;
     align-items: center;
     justify-content: space-between;
     gap: 1rem;
-    padding-top:5rem;
-    padding-left:0rem;
+    padding-top: 5rem;
+    padding-left: 0rem;
 
-    img,span{
+    img, span{
       font-size: 3rem;
       width: 3.5rem;
     }
   }
-
 
   .button{
     margin-top: 5rem;
@@ -48,32 +46,46 @@ export const Container = styled.section`
 
   .hero-image{
     img{
-      max-width: 500px;
-      margin-top: 0rem;
-      margin-right:8rem;
+      max-width: 100%; /* Adjusted to be responsive */
+      margin-top: -10rem;
+      margin-right: 16rem;
     }
   }
-
 
   @media(max-width: 960px){
     display: block;
     margin-top: 15%;
+    gap: 4rem; /* Adjusted gap for smaller screens */
+    
     .hero-text{
-
       h1{
         font-size: 5rem;
       }
     }
     
     .hero-image{
-      display:relative;
+      display: relative;
+      margin-top: 0rem;
+      margin-right: 0rem; /* Remove right margin on smaller screens */
     }
   }
 
   @media(max-width: 600px){
-    margin-top: 35%;
+    margin-top: 25%; /* Adjusted margin-top for smaller screens */
   }
+  
   @media(max-width: 480px){
-    margin-top: 45%;
+    margin-top: 35%; /* Adjusted margin-top for even smaller screens */
+    padding-top: 10%; /* Adjusted padding-top for better spacing */
+    
+    .hero-text{
+      & > p{
+        font-size: 2rem; /* Adjusted font size for smaller screens */
+      }
+    }
+    
+    .social-media{
+      padding-top: 2rem; /* Adjusted padding-top for smaller screens */
+    }
   }
-`
+`;
