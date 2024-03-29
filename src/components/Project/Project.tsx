732c34a323fa3bde7848f54b28a31287eb39bf8f
Niranjan <2100090187@kluniversity.in>
2024-03-29 19:07:00 +0530

    added project

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -225,6 +225,36 @@ Diwali project</h3>
             </footer>
           </div>
         </ScrollAnimation>
+
+
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+              <div className="project-links">
+                <a href="https://niranjansah87.com.np" target="_blank" rel="noreferrer">
+                  <img src={externalLink} alt="Visit site" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>KL Samarthya 2024</h3>
+              <p>
+              
+The Kl Samarthya Department Fest 2024 Website, powered by Django, offers a one-stop destination for everything related to the annual fest. From event details and registration for competitions to dynamic schedules and engaging galleries, the website ensures a seamless fest experience. Users can stay updated with news, participate through forums, and easily reach out to organizers. With its user-friendly interface and robust features, the website serves as the central hub for both organizers and attendees, enhancing the overall fest experience.
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>Django</li>
+                <li>Python</li>
+                <li>Html</li>
+                <li>css</li>
+                <li>JavaScript</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
        
 
       </div>
