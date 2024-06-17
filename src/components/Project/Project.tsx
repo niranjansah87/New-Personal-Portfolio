9ba0bd716c751df194c7477b2f4e3d7fb587c9e7
niranjan87 <2100090187csit@gmail.com>
2024-06-17 13:35:42 +0530

    added project

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -11,7 +11,32 @@ export function Project() {
       <h2>My Projects</h2>
       <div className="projects">
 
-  
+      <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+              <div className="project-links">
+                <a href="https://forensixplore.tech/" target="_blank" rel="noreferrer">
+                  <img src={externalLink} alt="Visit site" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>Forensixplore</h3>
+              <p>
+              Designed and developed the official website for ForensiXplore, our department's cybersecurity club. The site features a modern design, event calendar, resource library, membership portal, and blog section to enhance engagement and provide valuable resources to our community
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>React</li>
+                <li>Express</li>
+                <li>MongoDB</li>
+                <li>Nodejs</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
 
       <ScrollAnimation animateIn="flipInX">
           <div className="project">
