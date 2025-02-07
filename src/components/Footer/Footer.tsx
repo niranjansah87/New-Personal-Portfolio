52aea8b2e7e2057ae49dfef9784317635969ca2a
niranjan87 <2100090187csit@gmail.com>
2025-02-07 20:45:52 +0530

    abc

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,11 @@
 import { Container } from './styles'
-
 import reactIcon from '../../assets/react-icon.svg'
 import linkedin from '../../assets/linkedin.svg'
 import githubIcon from '../../assets/github.svg'
 import whatsapp from '../../assets/whatsapp.svg'
 import telegram from '../../assets/telegram.svg'
 import instagramIcon from '../../assets/instagram.svg'
-// import discordIcon from '../../assets/discord.png'
+
 
 export function Footer() {
   return (
